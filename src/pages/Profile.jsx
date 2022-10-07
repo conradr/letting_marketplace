fb8914e11080d207452e9a9b42520121045c9b59
Conrad Russo <conradr@gmail.com>
2022-10-07 20:45:12 +0100

    sign out button and logic

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,35 @@
 import { useState, useEffect } from 'react'
 
 import { getAuth } from 'firebase/auth'
+import { useNavigate, Link } from 'react-router-dom'
 
 const Profile = () => {
-  const [user, setUser] = useState(null)
-
   const auth = getAuth()
-  useEffect(() => {
-    setUser(auth.currentUser)
-  }, [])
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
 
-  return user ? <h1>Hi {user.displayName}</h1> : <h1>You're not logged in</h1>
-    
+  const { name, email } = formData
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
+  return (
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button type='button' className='logOut' onClick={onLogout}>
+          {' '}
+          Logout{' '}
+        </button>
+      </header>
+    </div>
+  )
 }
 
 export default Profile
